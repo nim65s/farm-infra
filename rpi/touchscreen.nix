2f1636df4fbfd69cc4125170c99374f3d9101676
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-11-26 11:24:41 +0100

    style
    /nix/store/pljyj762k3dmzysz5rlx8hq99iz16b07-udev-rules/99-local.rules:1 style: whitespace after comma is expected.

--- rpi/touchscreen.nix
+++ rpi/touchscreen.nix
@@ -10,9 +10,9 @@ in
   boot.extraModulePackages = [ pcanDriver ];
   boot.kernelModules = [ "pcan" ];
   services.udev.extraRules = ''
-    ENV{ID_VENDOR_ID}=="0eef",\
-    ENV{ID_MODEL_ID}=="c002",\
-    ENV{WL_INPUT}="DVI1",\
+    ENV{ID_VENDOR_ID}=="0eef", \
+    ENV{ID_MODEL_ID}=="c002", \
+    ENV{WL_INPUT}="DVI1", \
     ENV{LIBINPUT_CALIBRATION_MATRIX}="0 -1 1 1 0 0 0 0 1"
   '';
 }
